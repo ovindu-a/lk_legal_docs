b5aaab791f60c32d4bb6005bd053413a57cc6b62
nuuuwan <nuuuwan@gmail.com>
2025-07-08 16:12:07 +0530

    ~

--- src/lld/reports/ReadMe.py
+++ src/lld/reports/ReadMe.py
@@ -17,9 +17,7 @@ class ReadMe:
         self.time_str = TimeFormat.TIME.format(Time.now())
         self.doc_list = DocFactory.list_all()
         self.n_docs = len(self.doc_list)
-        self.total_data_size_m = (
-            DocFactory.get_total_data_size() / 1_000_000.0
-        )
+        self.total_data_size_m = DocFactory.get_total_data_size() / 1_000_000.0
         self.html_cache_size_m = WebPage.get_html_cache_size() / 1_000_000.0
         dates = [doc.date for doc in self.doc_list]
         self.min_date = min(dates)
@@ -192,7 +190,7 @@ class ReadMe:
             + f" for **{n_unique_docs:,}** documents ({p_docs:.1%})"
             + " have been downloaded."
             + " Final data size is estimated to be "
-            + f"{final_file_size_g:.0f} GB.",
+            + f"~{final_file_size_g:.0f} GB.",
             "",
         ]
 
