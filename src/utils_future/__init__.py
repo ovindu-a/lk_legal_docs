c194537513dbaea436a173689627b8442d010135
nuuuwan <nuuuwan@gmail.com>
2025-07-08 09:09:40 +0530

    Git

--- src/utils_future/__init__.py
+++ src/utils_future/__init__.py
@@ -2,5 +2,6 @@
 # flake8: noqa: F408
 
 from utils_future.Directory import Directory
+from utils_future.Git import Git
 from utils_future.Lang import Lang
 from utils_future.Markdown import Markdown
