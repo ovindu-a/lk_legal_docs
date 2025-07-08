644434e25eebdde664767c05e4709b1bcd92c195
nuuuwan <nuuuwan@gmail.com>
2025-07-08 17:00:42 +0530

    back-compressed pdfs

--- workflows/back_compress.py
+++ workflows/back_compress.py
@@ -1,6 +1,6 @@
 import os
 
-from lld import AbstractDoc
+from lld import AbstractDoc, DocFactory
 from utils_future import Git
 
 GIT_REPO_URL = "https://github.com/nuuuwan/lk_legal_docs_data.git"
@@ -10,10 +10,14 @@ def main():
     git = Git(AbstractDoc.DIR_TEMP_DATA)
     if not os.path.exists(AbstractDoc.DIR_TEMP_DATA):
         git.clone(GIT_REPO_URL)
-    else:
-        git.pull()
+    # else:
+    #     git.pull()
 
-    AbstractDoc.back_compress()
+    # AbstractDoc.back_compress()
+    doc_list = DocFactory.list_all()
+    for doc in doc_list:
+        doc.copy_metadata_to_temp_data()
+
     git.add(".").commit("back-compressed pdfs")
     git.pull()
     git.push()
