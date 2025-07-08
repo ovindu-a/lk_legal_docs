3980797b9e4e3c2e4f2565545334e668a00a7d14
nuuuwan <nuuuwan@gmail.com>
2025-07-08 19:29:59 +0530

    Generated ReadMe

--- src/lld/docs/abstract_doc/AbstractDocDownloader.py
+++ src/lld/docs/abstract_doc/AbstractDocDownloader.py
@@ -27,7 +27,9 @@ class AbstractDocDownloader:
 
     @cached_property
     def dir_temp_data(self):
-        return os.path.join(AbstractDocDownloader.DIR_TEMP_DATA, self.dir_data)
+        return os.path.join(
+            AbstractDocDownloader.DIR_TEMP_DATA, self.dir_data
+        )
 
     def get_pdf_path(self, lang):
         return os.path.join(self.dir_temp_data, f"{lang}.pdf")
@@ -67,7 +69,9 @@ class AbstractDocDownloader:
 
     @staticmethod
     def __gen_year_dirs__():
-        for doc_type_dir in AbstractDocDownloader.__gen_doc_type_dir_paths__():
+        for (
+            doc_type_dir
+        ) in AbstractDocDownloader.__gen_doc_type_dir_paths__():
             for year in os.listdir(doc_type_dir):
                 year_dir = os.path.join(doc_type_dir, year)
                 if not os.path.isdir(year_dir):
