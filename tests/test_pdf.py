7900bd992f0c90dec950fc1a3dcb3893bd5da21b
nuuuwan <nuuuwan@gmail.com>
2025-07-08 11:09:38 +0530

    tests

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -14,10 +14,15 @@ class TestCase(unittest.TestCase):
         if os.path.exists(output_path):
             os.remove(output_path)
 
-        PDF.compress_pdf(TEST_PDF_PATH, output_path)
+        PDF.compress(TEST_PDF_PATH, output_path)
 
         size_before = os.path.getsize(TEST_PDF_PATH)
         size_after = os.path.getsize(output_path)
 
         self.assertEqual(size_before, 339_235)
-        self.assertLess(size_after, size_before)
+        self.assertLess(size_after, size_before * 0.75)
+
+        PDF.compress(output_path, output_path)
+        size_after_again = os.path.getsize(output_path)
+        self.assertLess(size_after_again, size_after * 1.1)
+        self.assertGreater(size_after_again, size_after * 0.9)
