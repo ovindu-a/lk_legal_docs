0400da2901686c5fe4da8e9e98962de9cbe44c1c
nuuuwan <nuuuwan@gmail.com>
2025-07-09 14:23:54 +0530

    Updated title

--- src/lld/reports/ReadMe.py
+++ src/lld/reports/ReadMe.py
@@ -18,9 +18,7 @@ class ReadMe:
         self.time_str = TimeFormat.TIME.format(Time.now())
         self.doc_list = DocFactory.list_all()
         self.n_docs = len(self.doc_list)
-        self.total_data_size_m = (
-            DocFactory.get_total_data_size() / 1_000_000.0
-        )
+        self.total_data_size_m = DocFactory.get_total_data_size() / 1_000_000.0
         self.html_cache_size_m = WebPage.get_html_cache_size() / 1_000_000.0
         dates = [doc.date for doc in self.doc_list]
         self.min_date = min(dates)
@@ -220,7 +218,7 @@ class ReadMe:
         )
         return (
             [
-                "# Legal Documents - #SriLanka 🇱🇰",
+                "#  #SriLanka 🇱🇰 - Legal Documents #Dataset",
                 "",
                 f"*Last Updated **{self.time_str}**.*",
                 "",
