14a98b90a8c78b5e5137dc10ba92c56aef4ea566
nuuuwan <nuuuwan@gmail.com>
2025-07-10 10:13:53 +0530

    parallel UNTESTED

--- workflows/data_scraper.py
+++ workflows/data_scraper.py
@@ -2,7 +2,7 @@ import os
 import sys
 import time
 
-from utils import Log
+from utils import Log, Parallel
 
 from lld import AbstractDoc, DocFactory
 
@@ -10,6 +10,7 @@ DEFAULT_MAX_DELTA_T = 15 * 60
 
 
 log = Log("data_scraper")
+N_BATCH = 4
 
 
 def main(max_delta_t):
@@ -21,13 +22,31 @@ def main(max_delta_t):
     doc_list = DocFactory.list_all()
     n_doc_list = len(doc_list)
     log.debug(f"{n_doc_list=}")
-    for doc in doc_list:
-        log.debug(f"Processing {doc.id}...")
-        doc.copy_metadata_to_temp_data()
-        doc.download_all_pdfs()
-        doc.extract_text()
+
+    i_doc = 0
+
+    while i_doc < n_doc_list:
+        workers = []
+        for j in range(N_BATCH):
+            if i_doc >= n_doc_list:
+                break
+            doc = doc_list[i_doc]
+
+            def worker(doc):
+                doc.copy_metadata_to_temp_data()
+                doc.download_all_pdfs()
+                doc.extract_text()
+                return True
+
+            workers.append(worker(doc=doc))
+
+        Parallel.run(
+            workers,
+            max_threads=N_BATCH,
+        )
 
         delta_t = time.time() - t_start
+        log.debug(f"{delta_t=:,.1f}s")
         if delta_t > max_delta_t:
             log.warning(
                 f"⛔️ Stopping. ⏰ {delta_t:.1f}s > {max_delta_t:.1f}s."
